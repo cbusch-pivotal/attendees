59a3f7e9383944ef74b1a9b033ebcb920217245a
Chris Busch <cbusch@pivotal.io>
2016-06-05 09:30:42 -0500

    add jenkins script and pom update

---
+++ ci/jenkins.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e -x
+
+echo "Build number is: ${BUILD_NUMBER}"
+
+hostName=attendees
+appBase=${hostName}-v
+appName=${appBase}${BUILD_NUMBER}
+verHostname=${appName}
+cfDomain=pcf1.fe.gopivotal.com
+
+# find all existing apps starting with "attendees-v"
+deployedApps=$(cf apps | grep ${appBase} | cut -d" " -f1)
+#echo "Deployed app(s): $deployedApps"
+
+# push the new (green) version to PCF
+cf push $appName -n $verHostName
+
+# Smoke test before adding new green app to main app route
+echo "Do some smoke testing...."
+
+#*** Script should exit(!0) before this point, if a failure occurred ***
+
+# Map app version onto main app route and scale the app to support traffic
+# i.e. $ cf map-route attendees-v5 cfapps.io -n attendees
+echo "map ${appName} to route ${hostname}.${cfDomain}"
+cf map-route $appName $cfDomain -n $hostName
+
+# cf scale attendees-v5 -i 2
+echo "scaling ${appName} up..."
+cf scale $appName -i 5
+
+# Scale down, unmap routes, and remove old versions of app, except a basename app = attendees
+if [ ! -z "$deployedApps" -a "$deployedApps" != " " -a "$deployedApps" != "$appName" ]; then
+  echo "Performing zero-downtime cutover to $hostname"
+
+  while read -r line 
+  do
+    if [ ! -z "$line" -a "$line" != " " -a "$line" != "$appName" -a "$line" != "$hostname" ]; then 
+      echo "Scaling down, unmapping and removing app: $line"
+      cf scale "$line" -i 1
+      cf unmap-route "$line" $cfDomain -n $hostname
+      cf delete "$line" -f 
+    else
+      echo "Skipping $line" 
+    fi
+  done <<<"$deployedApps" 
+fi
