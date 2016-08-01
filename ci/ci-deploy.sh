8901abb622b8a54fd78588db3e31289ef7d9150e
Chris Busch <cbusch@pivotal.io>
2016-07-31 18:25:25 -0700

    comment script

--- ci/ci-deploy.sh
+++ ci/ci-deploy.sh
@@ -2,7 +2,12 @@
 
 set -e -x
 
+# login
 fly -t ccb login -c http://192.168.100.4:8080
+
+# create the pipeline
 fly -t ccb set-pipeline --pipeline pcf-ers-demo --config pipeline-new.yml --load-vars-from ~/.concourse/ci-props.yml
+
+# unpause so it can run
 fly -t ccb unpause-pipeline --pipeline pcf-ers-demo
 
