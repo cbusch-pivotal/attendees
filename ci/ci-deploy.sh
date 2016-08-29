3f64b07a394d8a91d77f842cf08ef05340fe21e3
Chris Busch <cbusch@pivotal.io>
2016-08-29 09:24:59 -0500

    update to attendees

--- ci/ci-deploy.sh
+++ ci/ci-deploy.sh
@@ -6,8 +6,8 @@ set -e -x
 fly -t ccb login -c http://192.168.100.4:8080
 
 # create the pipeline
-fly -t ccb set-pipeline --pipeline pcf-ers-demo --config pipeline-new.yml --load-vars-from ~/.concourse/ci-props.yml
+fly -t ccb set-pipeline --pipeline attendees-demo --config pipeline.yml --load-vars-from ~/.concourse/ci-props.yml
 
 # unpause so it can run
-fly -t ccb unpause-pipeline --pipeline pcf-ers-demo
+fly -t ccb unpause-pipeline --pipeline attendees-demo
 
